c5ab328bb19e4aadeb48b7c83b409abb403e41f0
KuharaNaoki <naoki.kuhara0928@gmail.com>
2021-11-28 01:16:01 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-const App = () => {
-    return <div>This is a template for react app.</div>;
-};
+// propsは親から子にデータを渡す時に使用するもの
+const App = props => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+  
+  return (
+    <>
+      <div>現在の{name}は、{price}円です</div>
+      {/* アロー関数にしないと無限ループになる  setPriceをそのまま呼び出すとrender => setState => render => setStateとなるから */}
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <br/>
+      <br/>
+      <input value={name} onChange={e => setName(e.target.value)} />
+    </>
+  )
+}
+
+// 外部からデータを読み込む場合
+// デフォルトのデータを定義
+App.defaultProps = {
+  name: "",
+  price: 1000
+}
 
 export default App;
