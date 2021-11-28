2ce192855bcecb19db889df4262b60456828e2cd
KuharaNaoki <naoki.kuhara0928@gmail.com>
2021-11-28 14:12:09 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 // propsは親から子にデータを渡す時に使用するもの
 const App = props => {
@@ -6,8 +6,21 @@ const App = props => {
   // 分割代入でstateの中身を代入しておくことでname, priceだけで値を使用できる
   const {name, price} = state;
 
-  // Resetのボタンの中身を外に出すと以下の記述になる
-  // const reset = () => setState(props)
+  // 第一引数に関数を受け取れる
+  // componentDidMountのような挙動をする。なので、Domが描画された後に実行される。
+  useEffect(() => {
+    console.log("This is like componentDidMount or componentDidUpdate.")
+  })
+
+  // 第２引数に空の配列を渡すと一度しか実行されないようになる
+  useEffect(() => {
+    console.log("This is like componentDidMount.")
+  }, [])
+
+  // 空の引数に反応したい値を入れると、その値が変更された時だけ実行されるようになる
+  useEffect(() => {
+    console.log("This callback is for name only.")
+  }, [name])
   
   return (
     <>
