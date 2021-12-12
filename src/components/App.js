247f78bae4ef8e338293f78fdb936fe303169d30
KuharaNaoki <naoki.kuhara0928@gmail.com>
2021-12-12 22:08:01 +0900

    一時保存

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reducer from '../reducers'
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addEvent = e => {
+    e.preventDefault()
+    // dispatch(action)
+    // action = {
+    // type属性が必ず必要
+    //    type: 'CREATE_EVENT',
+    //    title,
+    //    body
+    // }
+  }
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -16,7 +32,7 @@ const App = () => {
           <textarea className="form-control" id="formEventBody" />
         </div>
 
-        <button className="btn btn-primary mr-3">イベントを作成する</button>
+        <button className="btn btn-primary mr-3" onClick={addEvent}>イベントを作成する</button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
 
         <h4>イベント一覧</h4>
