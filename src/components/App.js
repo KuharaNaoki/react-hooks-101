4785293f10434c187488ab8c499b7590d805845c
KuharaNaoki <naoki.kuhara0928@gmail.com>
2022-01-01 01:47:53 +0900

    implement all events deletion feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,39 @@
 import React, { useReducer, useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import reducer from '../reducers'
-import Event from './Event'
+import "bootstrap/dist/css/bootstrap.min.css";
+import reducer from "../reducers";
+import Event from "./Event";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, [])
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
-  const addEvent = e => {
+  const addEvent = (e) => {
     // 「preventDefault」はイベントのデフォルトの動作を妨げている
     // フォームが持つデフォルトの動作とは、フォームの内容を指定したURLへ送信するという動作です。
-    e.preventDefault()
+    e.preventDefault();
 
     dispatch({
       // type属性が必ず必要
-      type: 'CREATE_EVENT',
+      type: "CREATE_EVENT",
       title,
-      body
-    })
+      body,
+    });
     // stateはイベント一覧の状態を管理している
     // 0: {id: 1, title: 't', body: 'b'}
     // 1: {id: 2, title: '', body: ''}
-    console.log({state})
-    setTitle('');
-    setBody('');
+    console.log({ state });
+    setTitle("");
+    setBody("");
+  };
+
+  const deleteAllEvents = e => {
+    e.preventDefault();
+    const result = window.confirm("Do you want to delete all data?");
+    if (result) dispatch({type: "DELETE_ALL_EVENTS" })
   }
+
+  const unCreatable = title === '' || body === ''
 
   return (
     <div className="container-fluid">
@@ -34,17 +42,26 @@ const App = () => {
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
           {/* 「e.target.value」は慣用句    setStateやsetTitleの関数を呼び出してtitleなどの状態を変更していく */}
-          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
+          <input
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
-
         <div className="form-group">
           <label htmlFor="formEventBody">ボディー</label>
-          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}/>
+          <textarea
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
         </div>
-
-        <button className="btn btn-primary mr-3" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
-
+        <button className="btn btn-primary mr-3" onClick={addEvent} disabled={unCreatable}>
+          イベントを作成する
+        </button>
+        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
         <h4 className="mt-3">イベント一覧</h4>
         <table className="table table-hover">
           <thead>
@@ -56,12 +73,14 @@ const App = () => {
             </tr>
           </thead>
           <tbody>
-            {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/> ))}
+            {state.map((event, index) => (
+              <Event key={index} event={event} dispatch={dispatch} />
+            ))}
           </tbody>
         </table>
       </form>
     </div>
-  )
+  );
 };
 
 export default App;
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const Event = ({event, dispatch}) => {
+const Event = ({ event, dispatch }) => {
   // const id = event.idと記述することで、dispatchの中にはidと記述するだけで良くなる
-  const id = event.id
-  const handleClickDeleteButton = e => {
-    e.preventDefault()
-    console.log("delete")
-    
-    dispatch({ type: 'DELETE_EVENT', id })
-  }
-  return <tr>
-    <td>{id}</td>
-    <td>{event.title}</td>
-    <td>{event.body}</td>
-    <td><button className="btn btn-danger" onClick={handleClickDeleteButton}>削除</button></td>
-  </tr>
-}
+  const id = event.id;
+  const handleClickDeleteButton = (e) => {
+    e.preventDefault();
+    console.log("delete");
+    const result = window.confirm(`Do you want to this data(id=${id})?`);
+    if (result) dispatch({ type: "DELETE_EVENT", id });
+  };
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{event.title}</td>
+      <td>{event.body}</td>
+      <td>
+        <button className="btn btn-danger" onClick={handleClickDeleteButton}>
+          削除
+        </button>
+      </td>
+    </tr>
+  );
+};
 
-export default Event
+export default Event;
